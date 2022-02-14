f0bdd3fc4b7b22816fec7eb64999c68927cb8005
shani <shhhaniburde@gmail.com>
2022-02-14 10:51:32 -0500

    Added seed data for artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,13 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+30.times {
+    Artist.create(
+        name: Faker::Name.name, 
+        genre: Faker::Music.genre, 
+        bio: Faker::Quote.yoda, 
+        image: "https://direct.rhapsody.com/imageserver/images/alb.20339423/500x500.jpg", 
+        followers: rand(100..100000)
+)}
+
 puts "✅ Done seeding!"
